241af57ca1e34a427924226006a50a98e01f8087
iAngga <agmcangga@gmail.com>
2022-02-28 00:25:16 +0700

    tambah lagi saya gan

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,114 @@
+/home/gawwsu/Music/1AM  Animal Crossing Wild World.mp3
+/home/gawwsu/Music/ARMADA - Asal Kau Bahagia.mp3
+/home/gawwsu/Music/BreakBeat Dut RemixDeatta Koro no You ni  Takagisan CV Rie TakahashiRamakun Remix.mp3
+/home/gawwsu/Music/bunny.mp3
+/home/gawwsu/Music/Dawin - Jumpshot (Dance Video).mp3
+/home/gawwsu/Music/espect.mp3
+/home/gawwsu/Music/espect.mp4
+/home/gawwsu/Music/Fourtwnty - Fana Merah Jambu.mp3
+/home/gawwsu/Music/Hime Pentako Funkot.mp3
+/home/gawwsu/Music/Payung Teduh - Akad.mp3
+/home/gawwsu/Music/Payung Teduh - Resah.mp3
+/home/gawwsu/Music/Payung Teduh - Untuk Perempuan Yang Sedang Di Pelukan.mp3
+/home/gawwsu/Music/[Remix Funkot] Colors☆Slash - Colors Power ni Omakasero! (TV).mp4
+/home/gawwsu/Music/Stephanie Poetri - I Love You 3000.mp3
+/home/gawwsu/Music/Tipsy.mp3
+/home/gawwsu/Music/Tulus - 1000 Tahun Lamanya.mp3
+/home/gawwsu/Music/Tulus - Jangan Cintai Aku Apa Adanya.mp3
+/home/gawwsu/Music/Tulus - Ruang Sendiri.mp3
+/home/gawwsu/Music/Tulus - Sepatu.mp3
+/home/gawwsu/Music/Tulus - Sewindu.mp3
+/home/gawwsu/Music/Tulus - Teman Hidup.mp3
+/home/gawwsu/Music/TWICE (트와이스) - What is Love.mp3
+/home/gawwsu/Music/Virtual Riot - Idols.mp4
+/home/gawwsu/Music/イワシがつちからはえてくるんだ.mp3
+/home/gawwsu/Music/クロマグロがとんでくる full.mp3
+/home/gawwsu/Music/ハレ晴レユカイ.mp3
+/home/gawwsu/Music/ユメの喫茶店 ミツキヨ.mp3
+/home/gawwsu/Music/好きだから- ユイカ.mp3
+/home/gawwsu/Music/廻廻奇譚 - Eve.mp3
+/home/gawwsu/Music/稲葉曇ラグトレインVo 歌愛ユキ.mp3
+/home/gawwsu/Music/Stay Alive 2019.mp3
+/home/gawwsu/Music/Gotoubun Funkot.mp3
+/home/gawwsu/Music/Kimi no Namaewa.mp3
+/home/gawwsu/Music/Rozakkun ft. Anoku-ID - Only My Railgun 2019 DB.mp3
+/home/gawwsu/Music/7!!_-_Lovers.mp3
+/home/gawwsu/Music/88RISING-Midsummer Madness.mp3
+/home/gawwsu/Music/Alan Walker - Faded.mp3
+/home/gawwsu/Music/Arizona Zervas- ROXANNE.mp3
+/home/gawwsu/Music/Basshunter - Dota.mp3
+/home/gawwsu/Music/CHEMISTRY_-_Period.mp3
+/home/gawwsu/Music/Chika_Dance.mp3
+/home/gawwsu/Music/mk_remix_claris_irony_reff_only_7wpFwp2aVxQ.mp3
+/home/gawwsu/Music/[OSTnime]_ClariS_-_Hitorigoto.mp3
+/home/gawwsu/Music/東方SSuuiikoott-dat file records.mp3
+/home/gawwsu/Music/Eir_Aoi_-_Memoria.mp3
+/home/gawwsu/Music/[OSTnime]_Eir_Aoi_-_Memoria.mp3
+/home/gawwsu/Music/Eve - KAIKAIKITAN.mp3
+/home/gawwsu/Music/FLOW_-_Sign.mp3
+/home/gawwsu/Music/Fourtwnty  Zona Nyaman.mp3
+/home/gawwsu/Music/Fukuhara_Miho_-_LET_IT_OUT.mp3
+/home/gawwsu/Music/BRADIO_-_Flyers.mp3
+/home/gawwsu/Music/Ren'ai Circulation by Kana Hanazawa [LaguAnime.XYZ].mp3
+/home/gawwsu/Music/Ikimono_Gakari_-_Blue_Bird.mp3
+/home/gawwsu/Music/Joji - Gimme Love.mp3
+/home/gawwsu/Music/Juice WRLD  Lucid Dreams.mp3
+/home/gawwsu/Music/kamaboko_colorful.mp3
+/home/gawwsu/Music/Lil Nas X-Old Town Road ft Billy Ray Cyrus.mp3
+/home/gawwsu/Music/LiSA_-_Ichiban_no_Takaramono.mp3
+/home/gawwsu/Music/LiSA_-_Oath_Sign.mp3
+/home/gawwsu/Music/Marshmello - Moving On.mp3
+/home/gawwsu/Music/Nakagawa_Shouko_-_RAY_OF_LIGHT.mp3
+/home/gawwsu/Music/NICO_Touches_the_Walls_-_Hologram.mp3
+/home/gawwsu/Music/Noisestorm - Crab Rave.mp3
+/home/gawwsu/Music/Petit_Rabbits_-_No_Poi.mp3
+/home/gawwsu/Music/Post Malone-Congratulations ft Quavo.mp3
+/home/gawwsu/Music/Post_Malone_-_White_Iverson.mp3
+/home/gawwsu/Music/Post Malone - Circles.mp3
+/home/gawwsu/Music/Post Malone - Goodbyes ft Young Thug.mp3
+/home/gawwsu/Music/Post Malone - "Wow.".mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/01. Paranoid.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/02. Spoil My Night (feat. Swae Lee).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/03. Rich & Sad.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/04. Zack and Codeine.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/05. Takin' Shots.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/06. rockstar (feat. 21 Savage).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/07. Over Now.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/08. Psycho (feat. Ty Dolla $ign).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/09. Better Now.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/10. Ball For Me (feat. Nicki Minaj).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/11. Otherside.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/12. Stay.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/13. Blame It On Me.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/14. Same Bitches (feat. G-Eazy & YG).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/15. Jonestown (Interlude).mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/16. 92 Explorer.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/17. Candy Paint.mp3
+/home/gawwsu/Music/Post Malone - beerbongs & bentleys (2018) Mp3 (320kbps) [Hunter]/Post Malone - beerbongs & bentleys (2018) [320]/18. Sugar Wraith.mp3
+/home/gawwsu/Music/Post Malone Swae Lee-Sunflower.mp3
+/home/gawwsu/Music/Post Malone - Motley Crew.mp3
+/home/gawwsu/Music/Post Malone and The Weeknd - One Right Now.mp3
+/home/gawwsu/Music/RevyMiko - Bon AppetittS (Funkot Remix).mp3
+/home/gawwsu/Music/Rhodanthe_-_Yumeiro_Parade.mp3
+/home/gawwsu/Music/Rich Brian - 100 Degrees.mp3
+/home/gawwsu/Music/SCANDAL_-_Shunkan_Sentimental.mp3
+/home/gawwsu/Music/SID_-_Uso.mp3
+/home/gawwsu/Music/Sid_-_Rain.mp3
+/home/gawwsu/Music/Kotonoha_no_Omoi_0.mp3
+/home/gawwsu/Music/Sukima_Switch_-_Golden_Time_Lover.mp3
+/home/gawwsu/Music/TEGRA39   Ramakun  Hatsune Miku - Akahitoha (Red Leaf).mp3
+/home/gawwsu/Music/happy_happy_sutorenji_furenzu_kiraramagic_remix.mp3
+/home/gawwsu/Music/[OSTnime]_TrySail_-_adrenaline.mp3
+/home/gawwsu/Music/twenty one pilots-Stressed Out.mp3
+/home/gawwsu/Music/YOASOBI_-_Yasashii_Suisei.mp3
+/home/gawwsu/Music/YOASOBI_-_Kaibutsu.mp3
+/home/gawwsu/Music/[OSTnime]_YUI_-_again.mp3
+/home/gawwsu/Music/YUI_-_again.mp3
+/home/gawwsu/Music/WONDERFUL_WONDER.mp3
+/home/gawwsu/Music/Nanawo_Akari_–_One_Room_Sugar_Life.mp3
+/home/gawwsu/Music/[Wataten_5_-_Kimama_na_Tenshitachi.mp3
+/home/gawwsu/Music/Wataten_5_-_Happy_Happy_Friends.mp3
+/home/gawwsu/Music/今宵mofumofu__.mp3
+/home/gawwsu/Music/もっふもふ_DE_よいのじゃよ.mp3
+/home/gawwsu/Music/Minami_-_Kawakiwo_Ameku.mp3
+/home/gawwsu/Music/ORDINARY_LOVE_0.mp3
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-test euy
+hayang nambah sesuatu jang di simpen didie ngan karek nyadar iye teh lain tempat backupan.
+
+
+sesuatu nu kuurng hasil nyudog
+
+- Collision Module -> https://github.com/Elv13/collision
+                                            
